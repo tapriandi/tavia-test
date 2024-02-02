40055ec7ecde92e716cf2eb292b501eda43d5f32
tapriandi <tapriandi@gmail.com>
2024-02-02 16:53:28 +0700

    chore: update style card product

--- src/Components/Atoms/Card.tsx
+++ src/Components/Atoms/Card.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { HDBIcon } from "../../assets";
-import "../../App.css";
 
 interface Props {
   data: any;
@@ -10,40 +9,45 @@ const Card = ({ data }: Props) => {
   const [showDesc, setShowDesc] = useState<boolean>(false);
 
   return (
-    <div >
-      <div >
-        <img src={data.pic} className="row" />
+    <div className="w-full bg-white font-light border shadow-md rounded-lg overflow-hidden duration-300 hover:shadow-sm">
+      <div className="h-80 group cursor-pointer relative overflow-hidden aspec-video md:h-64">
+        <div className="absolute inset-0 bg-black/10 opacity-0 duration-300 group-hover:opacity-100"></div>
+        <img src={data.pic} className="w-full object-cover object-center" />
       </div>
-      <div>
-        <div className="row">
+      <div className="p-5 lg:p-3">
+        <div className="flex justify-between lg:flex-col lg:justify-normal lg:items-start">
           <div>
-            <div className="row">
-              <img src={HDBIcon} />
+            <div className="flex space-x-4 pb-3 md:space-x-3">
+              <img src={HDBIcon} className="w-10 object-contain md:w-8" loading="lazy" />
               <div>
-                <p>{data.title}</p>
-                <p>{data.address}</p>
+                <p className="text-2xl font-bold text-black pb-1 lg:text-xl md:text-lg md:pb-0">
+                  {data.title}
+                </p>
+                <p className="text-sm md:text-xs">{data.address}</p>
               </div>
             </div>
             <div className="">
-              <p>
+              <p className="pb-1 md:text-sm">
                 {data.project_type} .{data.year} .{data.ownership_type}
               </p>
-              <p>{data.availabilities_label}</p>
+              <p className="md:text-sm">{data.availabilities_label}</p>
             </div>
           </div>
-
-          <div>
-            <p className="text-red-500">
+          <div className="text-right lg:text-left lg:pt-4">
+            <p className="flex font-bold pb-1 text-lg md:text-base">
               ${data.psf_min} - ${data.psf_min} psf
             </p>
-            <p>{data.subprice_label}</p>
+            <p className="text-sm md:text-sm">{data.subprice_label}</p>
           </div>
         </div>
 
         {/* footer */}
         <div>
-          {showDesc && <p>{data.description}</p>}
-          <div onClick={() => setShowDesc(!showDesc)}>
+          {showDesc && <p className="text-sm pt-4 md:text-xs">{data.description}</p>}
+          <div
+            onClick={() => setShowDesc(!showDesc)}
+            className="text-right pt-4 font-bold text-orange cursor-pointer text-orange-400 hover:underline md:text-xs md:pr-4 md:pb-1 md:font-medium"
+          >
             {showDesc ? "Hide description" : "See description"}
           </div>
         </div>
--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -5,10 +5,14 @@ import { Card } from "../../Components/Atoms";
 const index = () => {
   return (
     <>
-      <div style={{ display: "flex", flexWrap: 'wrap' }}>
-        {Data.map((item, index) => (
-          <Card key={index} data={item} />
-        ))}
+      <div className="pt-14 pb-20 lg:px-[4%]">
+        <div className="w-full px-[12%] lg:px-0">
+          <div className="grid grid-cols-2 gap-5 w-full lg:gap-4 md:grid-cols-1">
+            {Data.map((item, index) => (
+              <Card key={index} data={item} />
+            ))}
+          </div>
+        </div>
       </div>
     </>
   );
